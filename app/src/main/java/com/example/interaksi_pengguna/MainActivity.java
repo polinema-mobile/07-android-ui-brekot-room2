20b6a5b0517ed0514d3233f506b224d70b2a8e07
noviannurrohman <noviannurrohman99@gmail.com>
2020-09-29 16:57:30 +0700

    update kirim data

--- app/src/main/java/com/example/interaksi_pengguna/MainActivity.java
+++ app/src/main/java/com/example/interaksi_pengguna/MainActivity.java
@@ -3,24 +3,42 @@ package com.example.interaksi_pengguna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView editTanggalLahir, editNama, editNim;
+    TextView editTanggalLahir;
     Calendar calendar;
     DatePickerDialog datePickerDialog;
+
+    Button button;
+    EditText editNama,editNim;
+    RadioButton radioButton;
+    RadioGroup radioGroup;
+    Spinner spinnerJurusan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        button=findViewById(R.id.button);
+        editNama = (EditText) findViewById(R.id.editNama);
+        editNim = (EditText) findViewById(R.id.editNim);
+        spinnerJurusan = (Spinner) findViewById(R.id.spinnerJurusan);
 
         editTanggalLahir = (TextView)findViewById(R.id.editTanggalLahir);
 
@@ -41,5 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 datePickerDialog.show();
             }
         });
+
+
+
+        editNama=(EditText)findViewById(R.id.editNama);
+        editNim=(EditText)findViewById(R.id.editNim);
+        radioGroup=(RadioGroup) findViewById(R.id.radioGroup);
+        spinnerJurusan =(Spinner)findViewById(R.id.spinnerJurusan);
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View view) {
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton;
+        radioButton = (RadioButton) findViewById(selectedId);
+        switch (view.getId()){
+            case R.id.button:
+                Intent sendData = new Intent(MainActivity.this,MainActivity2.class);
+                sendData.putExtra("data1", editNama.getText().toString());
+                sendData.putExtra("data2", editNim.getText().toString());
+                sendData.putExtra("data3", editTanggalLahir.getText().toString());
+                sendData.putExtra("data4", radioButton.getText().toString());
+                sendData.putExtra("data5", spinnerJurusan.getSelectedItem().toString());
+
+                startActivity(sendData);
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/interaksi_pengguna/MainActivity2.java
+++ app/src/main/java/com/example/interaksi_pengguna/MainActivity2.java
@@ -3,12 +3,32 @@ package com.example.interaksi_pengguna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView datanama;
+    TextView datanim;
+    TextView datatgl;
+    TextView datajk;
+    TextView datajurusan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        datanama=(TextView)findViewById(R.id.datanama);
+        datanim=(TextView)findViewById(R.id.datanim);
+        datatgl=(TextView)findViewById(R.id.datatgl);
+        datajk=(TextView)findViewById(R.id.datajk);
+        datajurusan=(TextView)findViewById(R.id.datajurusan);
+
+        datanama.setText(getIntent().getStringExtra("data1"));
+        datanim.setText(getIntent().getStringExtra("data2"));
+        datatgl.setText(getIntent().getStringExtra("data3"));
+        datajk.setText(getIntent().getStringExtra("data4"));
+        datajurusan.setText(getIntent().getStringExtra("data5"));
+
     }
 }
